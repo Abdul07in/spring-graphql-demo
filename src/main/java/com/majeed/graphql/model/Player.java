91d7adae1281c4c29ed98a0bfe937ed512080930
Abdul_Majeed <abdulkanoor@gmail.com>
2026-01-04 23:57:10 +0530

    Initial commit

---
+++ src/main/java/com/majeed/graphql/model/Player.java
@@ -0,0 +1,4 @@
+package com.majeed.graphql.model;
+
+public record Player(Integer id, String name, Integer age, Team team) {
+}
---
+++ src/main/java/com/majeed/graphql/model/Team.java
@@ -0,0 +1,8 @@
+package com.majeed.graphql.model;
+
+public enum Team {
+    CSK, MI,
+    RCB, KKR,
+    SRH, DC,
+    RR, PBKS;
+}
